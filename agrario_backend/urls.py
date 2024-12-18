5afb3f7f0e82b35a494728c51ee041480927d210
“vstefan22” <stefwolf22@gmail.com>
2024-12-18 19:40:31 +0100

    admin panel url

--- agrario_backend/urls.py
+++ agrario_backend/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from accounts.views import MarketUserViewSet, ConfirmEmailView, LoginView
 from django.urls import path, include
+from django.contrib import admin
 
 router = DefaultRouter()
 router.register(r'users', MarketUserViewSet, basename='market-user')
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/accounts/', include(router.urls)),
     path('api/accounts/confirm-email/<uidb64>/<token>/',
          ConfirmEmailView.as_view(), name='confirm-email'),
