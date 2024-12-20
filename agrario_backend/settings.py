e2d84b68576bd894fa03d58e0f6d8c3a854879a6
mihailo50 <mihailocvetkovic2@gmail.com>
2024-12-20 17:40:42 +0100

    removed email credentials from settings.py

--- agrario_backend/settings.py
+++ agrario_backend/settings.py
@@ -91,9 +91,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'cuko534@gmail.com'
-EMAIL_HOST_PASSWORD = 'bkgc ykei urfu uctd'
-DEFAULT_FROM_EMAIL = 'Holograph'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'Agrario'
 
 ROOT_URLCONF = 'agrario_backend.urls'
 
