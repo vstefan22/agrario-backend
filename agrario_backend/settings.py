2e22a9016944df0158933b47e9a08b9607bc4217
mihailo50 <mihailocvetkovic2@gmail.com>
2025-01-04 15:59:35 +0100

    Messaging: - Unread count - Mark all as read - Retrieve messages - Send message - Inbox

--- agrario_backend/settings.py
+++ agrario_backend/settings.py
@@ -105,6 +105,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         "accounts.firebase_auth.FirebaseAuthentication"
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
--- messaging/admin.py
+++ messaging/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Message, Attachment, Chat
 
-# Register your models here.
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Attachment model.
+    """
+    list_display = ('file', 'uploaded_at')
+    list_filter = ('uploaded_at',)
+    search_fields = ('file',)
+    readonly_fields = ['uploaded_at']
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Message model.
+    """
+    list_display = ('subject', 'sender', 'get_recipient', 'created_at')  # Use a custom method
+    list_filter = ('created_at', 'sender', 'chat')  # Replace 'recipient' with valid fields
+    search_fields = ('subject', 'sender__email', 'body')  # Removed 'recipient__email'
+    ordering = ('-created_at',)
+
+    def get_recipient(self, obj):
+        return ", ".join([str(participant) for participant in obj.chat.user2.all()]) if obj.chat.user2 else "No Recipient"
+    get_recipient.short_description = "Recipient"
+
+    def get_queryset(self, request):
+        """
+        Ensure related objects are prefetched for performance.
+        """
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related('attachments')
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ("identifier", "created_at")
+    search_fields = ("identifier",)
+    list_filter = ("created_at",)
+
