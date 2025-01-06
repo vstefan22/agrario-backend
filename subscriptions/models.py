b0db6aeeeedc9c00b4dc54a3fbf31d0bcd8af406
gacicmihailo <gacicmihailo244@gmail.com>
2025-01-06 14:24:39 +0100

    fixing error for deployment and delete unnecessary requirements file

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from accounts.models import MarketUser, PaymentTransaction
+from accounts.models import MarketUser
+from payments.models import PaymentTransaction
 
 class PlatformSubscription(models.Model):
     """Instance of the available subscriptions"""
